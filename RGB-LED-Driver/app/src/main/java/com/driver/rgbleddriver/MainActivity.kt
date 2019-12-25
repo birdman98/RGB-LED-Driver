a09ea47c2f67eb2beff14d936d7b6c72f959f6f1
Piotr Matras <piotrmatras21@gmail.com>
2019-12-25 17:10:18 +0100

    main activity class building in progress...

--- RGB-LED-Driver/app/src/main/java/com/driver/rgbleddriver/MainActivity.kt
+++ RGB-LED-Driver/app/src/main/java/com/driver/rgbleddriver/MainActivity.kt
@@ -2,11 +2,23 @@ package com.driver.rgbleddriver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val musicReactiveBtn: Button = findViewById(R.id.musicReactiveBtn)
+        val rgbPickerBtn: Button = findViewById(R.id.rgbPickerBtn)
+
+        musicReactiveBtn.setOnClickListener { view ->
+            val espConnector: ESPConnector = ESPConnector("127.0.0.1", 1560)
+            espConnector.initializeConnection()
+            espConnector.run()
+
+        }
+
     }
 }
