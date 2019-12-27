7cbeec2907a7548652e3437d3eb3526fa88ac240
Piotr Matras <piotrmatras21@gmail.com>
2019-12-27 21:42:28 +0100

    added new activity for picking RGB color

---
+++ RGB-LED-Driver/app/src/main/java/com/driver/rgbleddriver/ColorPickerActivity.kt
@@ -0,0 +1,12 @@
+package com.driver.rgbleddriver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorPickerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_picker)
+    }
+}
